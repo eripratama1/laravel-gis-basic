cded0398dd96619ebfbf92a0259c51ee6f2aefdc
eripratama1 <eripratama90@gmail.com>
2023-07-10 23:01:14 +0800

    install datatable , create migration & views

---
+++ database/migrations/2023_07_10_081935_create_spots_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('coordinates');
+            $table->longText('description');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spots');
+    }
+};
