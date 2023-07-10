83a9aad4eb4e804e8e293589c3a1d39c082adc05
eripratama1 <eripratama90@gmail.com>
2023-07-10 15:10:03 +0800

    add menu manage data

--- resources/views/layouts/dashboard-volt.blade.php
+++ resources/views/layouts/dashboard-volt.blade.php
@@ -276,7 +276,59 @@
                             </li>
                         </ul>
                     </div>
-                </li>                               
+                </li>
+                
+                <li class="nav-item">
+                    <span
+                      class="nav-link  collapsed  d-flex justify-content-between align-items-center"
+                      data-bs-toggle="collapse" data-bs-target="#submenu-components">
+                      <span>
+                        <span class="sidebar-icon">
+                          <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"></path><path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        </span> 
+                        <span class="sidebar-text">Manage Data</span>
+                      </span>
+                      <span class="link-arrow">
+                        <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                      </span>
+                    </span>
+                    <div class="multi-level collapse " role="list"
+                      id="submenu-components" aria-expanded="false">
+                      <ul class="flex-column nav">
+                        <li class="nav-item">
+                          <a class="nav-link" target="_blank"
+                            href="https://themesberg.com/docs/volt-bootstrap-5-dashboard/components/accordions/">
+                            <span class="sidebar-text">All Components</span>
+                          </a>
+                        </li>
+                        <li class="nav-item ">
+                          <a class="nav-link" href="../../pages/components/buttons.html">
+                            <span class="sidebar-text">Buttons</span>
+                          </a>
+                        </li>
+                        <li class="nav-item ">
+                          <a class="nav-link" href="../../pages/components/notifications.html">
+                            <span class="sidebar-text">Notifications</span>
+                          </a>
+                        </li>
+                        <li class="nav-item ">
+                          <a class="nav-link" href="../../pages/components/forms.html">
+                            <span class="sidebar-text">Forms</span>
+                          </a>
+                        </li>
+                        <li class="nav-item ">
+                          <a class="nav-link" href="../../pages/components/modals.html">
+                            <span class="sidebar-text">Modals</span>
+                          </a>
+                        </li>
+                        <li class="nav-item ">
+                          <a class="nav-link" href="../../pages/components/typography.html">
+                            <span class="sidebar-text">Typography</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
                 <!--SIDEBAR MENU-->
 
                 <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
